70f7b5dc932aae22a3b47f81de56835eb7798c15
dl57934 <dl57934@pukyong.ac.kr>
2019-03-29 02:58:06 +0900

    Updates

---
+++ precache-manifest.ae29f66225b7bb7c5a98b605e4d55ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a841b18b56eb8c75e1a7",
+    "url": "/KoreanGrammarCheck/static/css/main.1f139aa5.chunk.css"
+  },
+  {
+    "revision": "a841b18b56eb8c75e1a7",
+    "url": "/KoreanGrammarCheck/static/js/main.bdaa5633.chunk.js"
+  },
+  {
+    "revision": "81b7fca3b3fb2f98a60a",
+    "url": "/KoreanGrammarCheck/static/js/runtime~main.7c9b5552.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/KoreanGrammarCheck/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/KoreanGrammarCheck/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ad869445cd83a13871adb3e4d131f852",
+    "url": "/KoreanGrammarCheck/index.html"
+  }
+];
