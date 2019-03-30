479de4bf0888c1f90810ca9f5ef9c8953b93263f
dl57934 <dl57934@pukyong.ac.kr>
2019-03-30 22:52:35 +0900

    edit result

--- galaxy-note/src/App.js
+++ galaxy-note/src/App.js
@@ -1,28 +1,59 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styled, { css } from "styled-components";
 
 class App extends Component {
+  state = {
+    inputText: ""
+  };
+
   render() {
+    const { inputText } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            asdasd <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MainContainer>
+        <TextArea value={inputText} onChange={this._onHandleInputText} />
+        <ChangeButton onClick={this._checkGrammar}>맞춤법 검사</ChangeButton>
+        <Result>{inputText}</Result>
+      </MainContainer>
     );
   }
+
+  _onHandleInputText = e => {
+    this.setState({ inputText: e.target.value });
+  };
+
+  _checkGrammar = () => {
+    console.log("huihi");
+  };
 }
 
 export default App;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const TextArea = styled.textarea`
+  ${inputAndResult}
+  width: 200px;
+  height: 100px;
+`;
+
+const Result = styled.div`
+  width: 200px;
+  height: 100px;
+  ${inputAndResult}
+  border: 1px black solid;
+`;
+
+const ChangeButton = styled.button`
+  display: flex;
+  width: 68px;
+  height: 30px;
+`;
+
+const inputAndResult = css`
+  width: 200px;
+  height: 100px;
+`;
