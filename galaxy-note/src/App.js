50104dfdbe34bc7cc83486e6fd8fa13e0d625564
dl57934 <dl57934@pukyong.ac.kr>
2019-04-01 01:10:31 +0900

    set JsDom

--- galaxy-note/src/App.js
+++ galaxy-note/src/App.js
@@ -1,7 +1,12 @@
 import React, { Component, Fragment } from "react";
 import styled, { css } from "styled-components";
+import cheerio from "cheerio";
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 class App extends Component {
+  statusTextColor = [];
+
   state = {
     inputText: ""
   };
@@ -17,22 +22,71 @@ class App extends Component {
             <InputStatus>{inputText.length}/500</InputStatus>
           </InputContainer>
           <ChangeButton onClick={this._checkGrammar}>검사</ChangeButton>
-          <TextArea
-            value={inputText}
-            onChange={this._onHandleInputText}
-            readOnly={true}
-          />
+          <ResultContainer>
+            <TextArea
+              value={inputText}
+              onChange={this._onHandleInputText}
+              readOnly={true}
+            />
+            <ResultStatusBox>
+              <dl style={{ marginRight: "10px" }}>
+                <ResultStatusOneLine>
+                  <ResultStatusCircle color="red" /> 맞춤법
+                </ResultStatusOneLine>
+                <ResultStatusOneLine>
+                  <ResultStatusCircle color="green" /> 띄어쓰기
+                </ResultStatusOneLine>
+              </dl>
+              <dl>
+                <ResultStatusOneLine>
+                  <ResultStatusCircle color="skyblue" /> 통계적 교정
+                </ResultStatusOneLine>
+                <ResultStatusOneLine>
+                  <ResultStatusCircle color="purple" /> 표준어 의심
+                </ResultStatusOneLine>
+              </dl>
+            </ResultStatusBox>
+          </ResultContainer>
         </MainContainer>
       </Fragment>
     );
   }
 
   _onHandleInputText = e => {
-    this.setState({ inputText: e.target.value });
+    const { value } = e.target;
+    if (value.length <= 500) this.setState({ inputText: e.target.value });
+    else alert("글자 수는 500글자가 최대입니다.");
   };
 
   _checkGrammar = () => {
-    console.log("huihi");
+    const window = new JSDOM("", {
+      url:
+        "https://search.naver.com/search.naver?sm=top_sug.pre&fbm=1&acr=1&acq=%EB%A7%9E%EC%B6%A4%EB%B2%95&qdt=0&ie=utf8&query=%EB%A7%9E%EC%B6%A4%EB%B2%95%EA%B2%80%EC%82%AC%EA%B8%B0"
+    }).window;
+    console.log(window.document.body.innerHTML);
+
+    // fetch(
+    //   "https://cors-anywhere.herokuapp.com/https://search.naver.com/search.naver?sm=top_sug.pre&fbm=1&acr=1&acq=%EB%A7%9E%EC%B6%A4%EB%B2%95&qdt=0&ie=utf8&query=%EB%A7%9E%EC%B6%A4%EB%B2%95%EA%B2%80%EC%82%AC%EA%B8%B0"
+    // )
+    //   .then(res => res.text())
+    //   .then(html => {
+    //     const window = new JSDOM(`${html}`, { runScripts: "outside-only" })
+    //       .window;
+    //     console.log(window.document.getElementsByTagName("textarea")[0].value);
+    //     console.log(
+    //       window.document.getElementsByTagName("textarea")[0].className
+    //     );
+    //     window.document.getElementsByTagName(
+    //       "textarea"
+    //     )[0].value = this.state.inputText;
+    //     console.log(window.document.getElementsByTagName("textarea")[0].value);
+    //     console.log(
+    //       window.document.getElementsByTagName("textarea")[0].className
+    //     );
+    //     // console.log(
+    //     //   window.document.getElementsByClassName("_result_text")[0].innerHTML
+    //     // );
+    //   });
   };
 }
 
@@ -50,13 +104,36 @@ const InputContainer = styled.div`
   flex-direction: column;
 `;
 
+const ResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ResultStatusBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: rgb(157, 163, 164);
+`;
+
+const ResultStatusCircle = styled.div`
+  width: 8px;
+  height: 8px;
+  background-color: ${props => props.color};
+  border-radius: 50%;
+  margin-top: 3px;
+`;
+
+const ResultStatusOneLine = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
 const InputStatus = styled.p`
   color: rgb(134, 139, 147);
   font-size: 15px;
 `;
 
 const Title = styled.h1`
-  /* padding-top: 20px; */
   margin-left: 20px;
   font-weight: bold;
   color: rgb(173, 178, 179);
